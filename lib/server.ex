50fab855ec98030caf301108cb7c9ca2ff82db3d
Joseph <joseph@josephbanks.me>
2018-06-05 23:16:41 +0100

    Remove need for Agent by using GenServer state

--- lib/server.ex
+++ lib/server.ex
@@ -16,15 +16,15 @@ defmodule Socket.Server do
 
     def handle_info({:loop, socket}, state) do
         {:ok, client} = :gen_tcp.accept(socket)
-        
+
         {:ok, pid} = Socket.RequestSupervisor.start_child([client])
         IO.puts "Handover return: " <> Kernel.inspect(:gen_tcp.controlling_process(client, pid))
         options = [
             active: true,
             packet: :http
-          ] 
+          ]
         IO.puts "setopts return: " <> Kernel.inspect(:inet.setopts(client, options))
-        
+
         send self(), {:loop, socket}
         {:noreply, state}
     end
@@ -32,22 +32,21 @@ defmodule Socket.Server do
 
     def send_response(status, body, socket, request, headers \\ %{}) do
         h = prepare_headers(request, headers)
-        
+
         response = "HTTP/1.1 " <> status <> "\r\n"
         response = response <> h
         response = response <> "\r\n\r\n" <> body
-        
+
         write_line(response, socket)
-        Socket.Agent.delete_request(socket)
         :gen_tcp.close(socket)
     end
 
-    def handle_request(request, socket, _genserver) do
+    def handle_request(request, socket) do
         Logger.debug request.path
         case request.path do
             '/' -> send_response("200 OK", "<h1>Hello webserver?</h1><a href='/test'>Hello?</a>", socket, request)
             '/test' -> send_response("200 OK", "<h1>Hello me again xd</h1>", socket, request)
-            '/user-agent' -> send_response("200 OK", "<p>You have a user agent of #{Map.get(request, :"User-Agent")}</p>", socket, request)
+            '/user-agent' -> send_response("200 OK", "<p>You have a user agent of #{Map.get(request.headers, :"User-Agent")}</p>", socket, request)
             '/redirect' -> send_response("200 OK", "<p>Hello redirect?</p>", socket, request, %{"Location": "https://seph.club/"})
             _ -> send_response("404 PAGE NOT FOUND", "", socket, request)
         end
@@ -67,7 +66,7 @@ defmodule Socket.Server do
         IO.inspect headers
         Enum.map(headers, fn x -> "#{elem(x, 0)}: #{elem(x, 1)}" end) |> Enum.join("\r\n")
     end
-    
+
     def write_line(line, socket) do
         :gen_tcp.send(socket, line)
     end
