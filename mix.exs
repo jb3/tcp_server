219dc7d86be8c21852fb21d159d0cd317e5d9c33
Joseph <joseph@josephbanks.me>
2018-06-07 02:48:12 +0100

    Application rename

--- mix.exs
+++ mix.exs
@@ -1,9 +1,9 @@
-defmodule Socket.Mixfile do
+defmodule TCPServer.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :socket,
+      app: :tcp_server,
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
@@ -15,7 +15,7 @@ defmodule Socket.Mixfile do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Socket, []}
+      mod: {TCPServer, []}
     ]
   end
 
