60fb18c41d7a4b8d071ffd34c3ace35d514b6f5f
Joseph Banks <joseph@josephbanks.me>
2018-09-16 01:04:00 +0100

    Config format

--- config/config.exs
+++ config/config.exs
@@ -28,5 +28,4 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env}.exs"
-config :logger, :console,
-  format: "$time $metadata[$level] $levelpad$message\n"
+config :logger, :console, format: "$time $metadata[$level] $levelpad$message\n"
